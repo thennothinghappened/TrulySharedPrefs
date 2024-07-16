82ba0976024bf778f3714cc42a623bc782e51ce3
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-07-16 16:29:59 +1000

    feat: `1.2.1`

--- trulysharedprefs/build.gradle.kts
+++ trulysharedprefs/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 group = "org.orca.trulysharedprefs"
-version = "1.2.0"
+version = "1.2.1"
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
